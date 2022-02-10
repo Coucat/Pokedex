a165600878a7369d534fb29427cf7b07b0064581
Emmanuelcazlo <Emmanuelcazlo@gmail.com>
2022-02-09 21:57:20 -0600

    Recreated project

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"

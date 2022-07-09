1ff1276134aed791bbefd02060355aad0f1397d8
pablo179 <cm.pablo.prog@gmail.com>
2022-07-09 16:50:03 -0500

    in progress

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from './componets/Header';
 import SearchNav from './componets/SearchNav';
 import Pokemons from './componets/pokemonCard';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SearchNav />
-      <Pokemons />
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Pokedex</title>
+        <meta
+          name="description"
+          content="Pokedex with all the information you need, list of Pokemons with stats, check out your favorite Pokemon."
+          keywords="pokemon, Pokemons, pokedex, stats, type, pikachu, game, pokeball"
+        />
+      </Helmet>
+      <div className="App">
+        <Header />
+        <SearchNav />
+        <Pokemons />
+      </div>
+    </HelmetProvider>
   );
 }
 
